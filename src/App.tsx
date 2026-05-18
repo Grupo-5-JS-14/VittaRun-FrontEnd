72034a62d8f86169c21492e32550f54707a8f809
Bruna Zuppini <brunazuppini@gmail.com>
2026-05-18 13:45:53 -0300

    feat: adiciona footer e navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Footer from './components/footer/Footer';
+import Navbar from './components/nav/Nav'
+
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Footer />
     </>
   )
 }
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,10 @@
+import Grupo05 from "../../components/grupo05/Grupo05"
+
 function About() {
   return (
     <div>About</div>
+
+    <Grupo05 variante="grande" />
   )
 }
 
